d0cddbaca594209bb1b320707327949a7b275d14
mythaouel <thaottm19411@st.uel.edu.vn>
2021-12-12 11:01:18 +0700

    sự kiện menu

--- app/src/main/java/com/n04/g701/AddProduct.java
+++ app/src/main/java/com/n04/g701/AddProduct.java
@@ -3,12 +3,49 @@ package com.n04.g701;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.n04.model.Product;
 
 public class AddProduct extends AppCompatActivity {
+
+    EditText edtName,edtPrice;
+    Button btnOk, btnCancel;
+
+    Product p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        linkViews();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void linkViews() {
+        edtName = findViewById(R.id.edtName);
+        edtPrice = findViewById(R.id.edtPrice);
+
+        btnOk = findViewById(R.id.btnOk);
+        btnCancel = findViewById(R.id.btnCancel);
     }
 }
--- app/src/main/java/com/n04/g701/EditProduct.java
+++ app/src/main/java/com/n04/g701/EditProduct.java
@@ -3,12 +3,46 @@ package com.n04.g701;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditProduct extends AppCompatActivity {
+
+    EditText edtName,edtPrice;
+    Button btnOk, btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_product);
+
+        linkViews();
+        addEvents();
+    }
+
+    private void linkViews() {
+        edtName = findViewById(R.id.edtName);
+        edtPrice = findViewById(R.id.edtPrice);
+
+        btnOk = findViewById(R.id.btnOk);
+        btnCancel = findViewById(R.id.btnCancel);
+    }
+
+    private void addEvents() {
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Thêm dữ liệu
+
+            }
+        });
     }
 }
