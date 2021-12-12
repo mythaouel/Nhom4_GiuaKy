d7e578f2917ea1c033f9502179fb2a51f4c49d67
yenhk1305 <92587395+yenhk1305@users.noreply.github.com>
2021-12-12 16:37:35 +0700

    sualai

--- app/src/main/java/com/n04/g701/MainActivity.java
+++ app/src/main/java/com/n04/g701/MainActivity.java
@@ -2,7 +2,6 @@ package com.n04.g701;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
@@ -12,47 +11,11 @@ import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     MyDatabaseHelper db;
-=======
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-public class MainActivity extends AppCompatActivity {
-
-    ListView lvProduct;
-
-    public static MyDatabaseHelper db;
->>>>>>> dff8f6fdc6c0288265c13f813a79a7de38faac8a
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db= new MyDatabaseHelper(this);
-<<<<<<< HEAD
         db.createSomeDefaultTasks();
-=======
-
-        linkViews();
-        addEvents();
-    }
-
-    private void linkViews() {
-        lvProduct = findViewById(R.id.lvProduct);
-    }
-    private void addEvents() {
-        lvProduct.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                openProductDetail();
-            }
-        });
-    }
-
-    private void openProductDetail() {
-        Intent intent = new Intent(this, ProductDetail.class);
-        startActivity(intent);
->>>>>>> dff8f6fdc6c0288265c13f813a79a7de38faac8a
     }
 }
