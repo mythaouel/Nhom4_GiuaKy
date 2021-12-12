3e382e7432afab748b5d851e1034268e81130cc4
thaonmp <thaonmp19411@st.uel.edu.vn>
2021-12-12 11:00:09 +0700

    layout chi tiết sản phẩm

---
+++ app/src/main/java/com/n04/g701/ProductDetail.java
@@ -0,0 +1,14 @@
+package com.n04.g701;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
