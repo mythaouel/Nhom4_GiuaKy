cd19b80f965a209f56d046ad69f0ecf16af90dc9
lnghle306uel <lelnh19411@st.uel.edu.vn>
2021-12-12 10:48:39 +0700

    push lại model với adapter

--- app/src/main/java/com/n04/g701/MainActivity.java
+++ app/src/main/java/com/n04/g701/MainActivity.java
@@ -3,20 +3,12 @@ package com.n04.g701;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lvProduct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        linkViews();
-    }
-
-    private void linkViews() {
-
     }
 }
